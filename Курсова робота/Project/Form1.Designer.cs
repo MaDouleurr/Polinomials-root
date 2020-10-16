23659920fe133cb2eea042fb6fc5cab430042852
MaDouleurr <60894148+MaDouleurr@users.noreply.github.com>
2020-10-16 23:27:30 +0300

    Initial commit

---
+++ Курсова робота/Project/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(58, 75);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Enter);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.Color.LightBlue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.label1.Location = new System.Drawing.Point(58, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введіть степінь Вашого поліному:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(214, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Згенерувати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(1, 106);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 50);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поліном";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(125, 261);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button2.Size = new System.Drawing.Size(96, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Розв\'язати";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(541, 261);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 28);
+            this.button3.TabIndex = 5;
+            this.button3.Tag = "";
+            this.button3.Text = "Розв\'язати";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(98, 159);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(167, 28);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Метод Горнера";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(523, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Метод Лобачевського";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox2.Location = new System.Drawing.Point(27, 308);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(322, 244);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Відповідь:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox3.Location = new System.Drawing.Point(431, 308);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(322, 244);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Відповідь:";
+            this.groupBox3.Enter += new System.EventHandler(this.groupBox3_Enter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(602, 238);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.LightBlue;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(479, 216);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 40);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Введіть точність:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.MintCream;
+            this.ClientSize = new System.Drawing.Size(800, 613);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Cursor = System.Windows.Forms.Cursors.PanNE;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Tag = "";
+            this.Text = "My course work";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Курсова робота/Project/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    
+
+    public partial class Form1 : Form{
+        TextBox[] tb; //масив текст боксів длч полінома
+        Label[] Lm;   //масив лейблів для полінома
+        Label[] Lg;
+        Label[] LL;
+        private double[] cor;//polinom
+        private double[] cr;// masiv corniv
+        private double[] crl;
+        public int cnt_text = 0; //кількість ячейок
+        public Form1() //конструктор форм
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //створюємо ячейки
+        {
+            if (cnt_text != 0)
+                groupBox1.Controls.Clear(); //чистить все поле для ячейок
+            tb = new TextBox[(int)numericUpDown1.Value+1]; //ініціалізуємо масив текст боксів
+            Lm = new Label[(int)numericUpDown1.Value+1]; // /-/ лейблів для полінома
+            cnt_text = (int)numericUpDown1.Value+1; 
+            for (int i = 0; i < cnt_text; i++) // функція яка малює ячейки та поля
+            {
+                Lm[i] = new Label();
+                groupBox1.Controls.Add(Lm[i]);
+                Lm[i].Location = new Point(56 + i * 81, 20);
+                Lm[i].Size = new Size(35, 20);
+                Lm[i].Text = "X^" + (cnt_text - i-1)+"+";
+                tb[i] = new TextBox();
+                groupBox1.Controls.Add(tb[i]);
+                tb[i].Location = new Point(15 + i * 81, 20 );
+                tb[i].Size = new Size(35, 20);
+            }
+            Lm[cnt_text - 1].Text="= 0" ;
+            button2.Enabled = true;
+            button3.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+                button1_Click(sender, e);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)//gorner
+        {
+            Gorner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && !have_char(textBox1.Text)) // перевірка якщо пусте місце то нуль
+                Lobachevskiy(1.0/ Convert.ToDouble(textBox1.Text));
+            else Lobachevskiy(0.00000001);    
+        }
+        private void Enter(object sender, KeyPressEventArgs e)
+        {
+
+        }
+        public bool have_char( string A)
+        {
+            for (int i = 0; i < A.Length; i++)
+                if (A[i] < 48 || A[i] > 57)
+                    if(A[i]!=43&&A[i]!=45)
+                        return true;
+            return false;
+        }
+        public void Gorner()
+        {
+            groupBox2.Controls.Clear();
+            cor = new double[cnt_text]; //масив полінома
+            cr = new double[cnt_text]; //масив корнів
+            for (int i = 0; i < cnt_text; i++) //зчитує поля з текст боксів полінома
+                if (tb[i].Text != ""&& !have_char(tb[i].Text)) // перевірка якщо пусте місце то нуль
+                    cor[i] = Convert.ToDouble(tb[i].Text); //////пошукати методи щоб перевіряло чи це цифра чи ні??????
+                else cor[i] = 0;
+            int cnt_cor =0; //кількісь корнів
+            double[] corn = new double[cor.Length]; //записуєм наш поліном
+            Array.Copy(cor, 0, corn, 0, cor.Length);
+            int cnt = cor.Length - 1; //схема горнера дальше
+            if (corn[cnt] == 0) //перевірка чи корень 0
+            {
+                cr[cnt_cor++] = 0;
+                cnt--;
+                for (int i = cnt;cnt!=-1 ; i--)
+                {
+                    if (corn[cnt] == 0) { 
+                        cnt--;
+                  
+                        } 
+                        else
+                    {
+                        break;
+                    }
+                }
+                
+            }
+            if (cnt != -1)
+            {
+                //cnt_cor = 0;
+                for (int j = 1; j <= Math.Abs(corn[cnt]); j++)
+                {
+
+                    if (corn[cnt] == ((int)corn[cnt] / j) * j)
+                    {
+                        if (coren(j) == 0)
+                        {
+                            cr[cnt_cor++] = j;
+                            for (int i = 1; i < cor.Length; i++)
+                            {
+                                corn[i] += corn[i - 1] * j;
+
+                            }
+                            cnt--;
+
+
+                        }
+
+                        if (coren(-j) == 0)
+                        {
+                            cr[cnt_cor++] = -j;
+                            for (int i = 1; i < cor.Length; i++)
+                            {
+                                corn[i] += corn[i - 1] * (-j);
+
+                            }
+                            cnt--;
+
+                        }
+                    }
+
+                }
+            }
+          if ((cr.Length) != cnt_cor)
+                Array.Resize(ref cr, cnt_cor);
+            Lg = new Label[cnt_cor];
+            int o = 0;
+            for (int i = 0; i < cnt_text; i++)
+                if (cor[i] != 0)
+                {
+                    o = 1;
+                    break;
+                }
+            if(o==1)
+            for (int i = 0; i < cr.Length; i++)
+            {
+                Lg[i] = new Label();
+                groupBox2.Controls.Add(Lg[i]);
+                Lg[i].Location = new Point(10, 10 + i * 30 + 5);
+                Lg[i].Size = new Size(50, 20);
+                Lg[i].Text = "X" + (i+1) + "= "+cr[i]; // записуємо наші корні в текст бокс
+
+            }
+            if (o == 0||cnt_cor==0)
+            {
+                Lg = new Label[1];
+                Lg[0] = new Label();
+                groupBox2.Controls.Add(Lg[0]);
+                Lg[0].Location = new Point(10, 10 + 1 * 30 + 5);
+                Lg[0].Size = new Size(400, 20);
+                Lg[0].Text = "Net korney"; // записуємо наші корні в текст бокс
+            }
+            return;
+        }
+
+        public double coren(double x) //функція перевірки на корінь
+        {
+            double k = 0;
+            for (int i = (int)cor.Length ; i >= 1; i--)
+            {
+                k += (cor[i-1] * Math.Pow(x, cor.Length - i));
+            }
+            if (Double.IsNaN(x))
+                return 1000;
+            return k;
+        }
+        void Lobachevskiy(double eps)
+        {
+            groupBox3.Controls.Clear();
+            int z = 0;
+            cor = new double[cnt_text];
+            for (int i = 0; i < cnt_text; i++)
+                if (tb[i].Text != ""&&!have_char(tb[i].Text))
+                    cor[i] = Convert.ToDouble(tb[i].Text);
+                else cor[i] = 0;
+            int cnt_cor = 0;
+            double [] crl = new double[cnt_text-1]; //корні
+            double[] m1 = new double[cnt_text]; //масив которий зберігає змінений поліном
+            Array.Copy(cor, 0, m1, 0, cor.Length);
+            double[] m2 = new double[cor.Length];
+            double[] cr2 = new double[cor.Length-1];
+            for (int i = 0; i < (int)crl.Length; i++) //знаходить початкові корні
+                crl[i] = m1[i + 1] / m1[i];
+            int cnt_for = 0;
+            for (int cnt = 2;cnt_for<1000 ; cnt *= 2,cnt_for+=5) //процес квадрування і знаходження корнів
+            {
+                for (int i = 0; i < (int)cor.Length; i++)
+                {
+                    m2[i] = m1[i] * m1[i];
+
+                    for (int j = 1, l = 1; i - j >= 0 && ((i + j) < (int)cor.Length); j++, l++)
+                    {
+                        m2[i] += Math.Pow(-1, l) * 2 * m1[i - j] * m1[i + j];
+                    }
+                }
+                for (int i = 0; i < (int)crl.Length; i++)
+                {
+                    cr2[i] = Math.Pow((m2[i + 1] / m2[i]), (double)1 / cnt);
+                    if(coren(cr2[i])<1||coren(-cr2[i])<1)
+                    if (Math.Abs(cr2[i] - crl[i]) < eps && cnt != 2)
+                    {
+                        for (int k = 0; k < (int)crl.Length; k++)
+                        {
+
+                            if (Math.Abs(coren(cr2[k])) > 1)
+                            {
+                                if (Math.Abs(coren(-cr2[k])) < 1 && !Double.IsNaN(coren(cr2[k])))
+                                {
+                                    crl[z++] = -cr2[k];
+                                }
+                                continue;
+
+                            }
+                            if(!Double.IsNaN(coren(cr2[k])))
+                             crl[z++] = cr2[k];
+                        }
+                            //Array.Resize(ref crl, crl.Length-z-1);
+                        LL = new Label[z];
+                        for (int k = 0; k < z; k++)
+                        {
+                            LL[k] = new Label();
+                            groupBox3.Controls.Add(LL[k]);
+                            LL[k].Location = new Point(10, 10 + k * 30+5);
+                            LL[k].Size = new Size(400, 20);
+                            LL[k].Text = "X" + (k+1) + "= " + crl[k]+"   "+coren(crl[k]);
+                        }
+                        
+                        return;
+                    }
+                }
+                Array.Copy(m2, 0, m1, 0, m1.Length);
+                Array.Copy(cr2, 0, crl, 0, crl.Length);
+            }
+
+            LL = new Label[1];
+            LL[0] = new Label();
+            groupBox3.Controls.Add(LL[0]);
+            LL[0].Location = new Point(10, 10 + 1 * 30+5);
+            LL[0].Size = new Size(400, 20);
+            LL[0].Text = "net korney, bolshaya tochnost ili nuli"+cnt_for;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    };
+}
+
